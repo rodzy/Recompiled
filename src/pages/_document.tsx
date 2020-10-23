4d84ad952037e9ecb41fb2df2c3854be382372bd
rodzy <49137701+rodzy@users.noreply.github.com>
2020-10-23 15:56:11 -0600

    Favicon issues

---
+++ src/pages/_document.tsx
@@ -0,0 +1,37 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <meta
+                        httpEquiv="Content-Type"
+                        content="text/html; charset=utf-8"
+                    />
+                    {/* Twitter tags */}
+                    <meta name="twitter:card" content="summary" />
+                    <meta name="twitter:creator" content="@rodzyrm" />
+                    <meta
+                        name="twitter:image"
+                        content="https://avatars1.githubusercontent.com/u/49137701?s=400&u=35e7614cc3215c045b58b4e352bd0616b9d795fd&v=4"
+                    />
+                    {/*Open graph tags */}
+                    <meta name="og:url" content="https://rodzy.vercel.app" />
+                    <meta name="og:site_name" content="Recompiled" />
+                    <meta
+                        property="og:image"
+                        content="https://avatars1.githubusercontent.com/u/49137701?s=400&u=35e7614cc3215c045b58b4e352bd0616b9d795fd&v=4"
+                    />
+                    <link rel="icon" href="/public/favicon-rm.ico" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
