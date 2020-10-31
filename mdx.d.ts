0d574733e3a91812a92c9e74044a59789b0c43ec
rodzy <49137701+rodzy@users.noreply.github.com>
2020-10-31 12:22:25 -0600

    Updated mdx type definitions

--- mdx.d.ts
+++ mdx.d.ts
@@ -1,4 +1,9 @@
+interface FrontMatter {
+    __resourcePath: string;
+}
+
 declare module "*.mdx" {
     let MDXComponent: (props: any) => JSX.Element;
     export default MDXComponent;
+    export const frontMatter: FrontMatter[];
 }
